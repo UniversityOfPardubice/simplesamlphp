436ee6cf2fa5b6e2eb7d493665da77d05cfd75c0
Lukas Slansky <lukas.slansky@gmail.com>
2015-01-28 13:16:40 +0100

    Aktualizace na 1.13

--- config/ldapmulti.php
+++ config/ldapmulti.php
@@ -3,7 +3,6 @@
 /* 
  * Configuration for the multi-DN LDAP authentication module.
  * 
- * $Id$
  */
 
 $ldapmulti = array (
@@ -14,7 +13,7 @@ $ldapmulti = array (
 		'dnpattern'			=> 'uid=%username%,dc=feide,dc=no,ou=feide,dc=uninett,dc=no',
 		'hostname'			=> 'ldap.uninett.no',
 		'attributes'		=> NULL,
-		'enable_tls'		=> FALSE,
+		'enable_tls'		=> TRUE,
 		'search.enable'		=> FALSE,
 		'search.base'		=> NULL,
 		'search.attributes'	=> NULL,
